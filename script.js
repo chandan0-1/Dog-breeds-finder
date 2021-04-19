42d50bf546e3d76c5be23988fbee50ed99f2d37e
chandan0-1 <chandanofficial.cn@gmail.com>
2021-04-19 20:48:57 +0530

    making ajax request using Jquery

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 
+// -------------Method - 1
+// function fetchImage(){
+// 	var xhrRequest = new XMLHttpRequest();
+
+// 	xhrRequest.onload = function(){
+// 		console.log(xhrRequest.response);
+// 		var resposeJSON = JSON.parse(xhrRequest.response);
+// 		var imgurl = resposeJSON.message;
+
+// 		$('#image').attr("src",imgurl);
+// 	}
+
+// 	xhrRequest.open('get',"https://dog.ceo/api/breeds/image/random" );
+// 	xhrRequest.send();
+// }
+
+                            // Method 2
+
+// $.ajax({
+// 	url:"https://dog.ceo/api/breeds/image/random",
+// 	method:"get",
+// 	success:function(data){
+
+// 		var imgurl = data.message;
+// 		$('#image').attr("src",imgurl);
+// 	}
+// })
+
+						// Method 3
 
 function fetchImage(){
-	var xhrRequest = new XMLHttpRequest();
-
-	xhrRequest.onload = function(){
-		console.log(xhrRequest.response);
-		var resposeJSON = JSON.parse(xhrRequest.response);
-		var imgurl = resposeJSON.message;
-
+	$.get("https://dog.ceo/api/breeds/image/random",function(data){
+		var imgurl = data.message;
 		$('#image').attr("src",imgurl);
-	}
-
-	xhrRequest.open('get',"https://dog.ceo/api/breeds/image/random" );
-	xhrRequest.send();
+	})
 }
-
-
 
 
 
