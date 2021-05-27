fbf0489cb13c2bfb364354bc33fbc21b42ddc1f1
chandan0-1 <65819925+chandan0-1@users.noreply.github.com>
2021-05-27 19:13:16 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
 
+// -------------Method - 1
+// function fetchImage(){
+// 	var xhrRequest = new XMLHttpRequest();
+
+// 	xhrRequest.onload = function(){
+// 		console.log(xhrRequest.response);
+// 		var resposeJSON = JSON.parse(xhrRequest.response);
+// 		var imgurl = resposeJSON.message;
+
+// 		$('#image').attr("src",imgurl);
+// 	}
+
+// 	xhrRequest.open('get',"https://dog.ceo/api/breeds/image/random" );
+// 	xhrRequest.send();
+// }
+
+                            // Method 2
+
+// $.ajax({
+// 	url:"https://dog.ceo/api/breeds/image/random",
+// 	method:"get",
+// 	success:function(data){
+
+// 		var imgurl = data.message;
+// 		$('#image').attr("src",imgurl);
+// 	}
+// })
+
+						// Method 3
+
+// For initial loading 
+$.get(`https://dog.ceo/api/breeds/image/random`,function(data){
+		var imgurl = data.message;
+		$('#image').attr("src",imgurl);
+	})
+
+
+
+// Getting all breed list 
+$.get("https://dog.ceo/api/breeds/list/all", function(data){
+	var list = data.message
+	for (let v in list){
+		$("#drp-down").append(`<option>${v}</option>`)
+	}
+})
+
 
 function fetchImage(){
-	var xhrRequest = new XMLHttpRequest();
-
-	xhrRequest.onload = function(){
-		console.log(xhrRequest.response);
-		var resposeJSON = JSON.parse(xhrRequest.response);
-		var imgurl = resposeJSON.message;
-
+	var Val = $("#drp-down").val();
+	console.log(Val)
+	$.get(`https://dog.ceo/api/breed/${Val}/images/random`,function(data){
+		var imgurl = data.message;
 		$('#image').attr("src",imgurl);
-	}
-
-	xhrRequest.open('get',"https://dog.ceo/api/breeds/image/random" );
-	xhrRequest.send();
+	})
 }
-
-
-
-
-
 
 $('#button').click(fetchImage);
